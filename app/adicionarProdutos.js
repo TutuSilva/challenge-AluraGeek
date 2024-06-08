9f92712597ebccc145da7efd1d31487ccc6a7ab0
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-06-08 09:22:51 -0300

    Update adicionarProdutos.js

--- app/adicionarProdutos.js
+++ app/adicionarProdutos.js
@@ -2,6 +2,7 @@ import { conectaApi } from "./conectaApi.js";
 const formulario = document.querySelector("[data-formulario]");
 
 async function adicionarProdutos(evento) {
+  evento.preventDefault();
   const nome = document.querySelector("[data-nomeProduto]").value;
   const preco = document.querySelector("[data-precoProduto]").value;
   const imagem = document.querySelector("[data-imagemProduto]").value;
