358558a4366dc25f43d8cb3eafe90a667ce8a2c7
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-06-08 09:15:05 -0300

    Update conectaApi.js

--- app/conectaApi.js
+++ app/conectaApi.js
@@ -1,28 +1,36 @@
 async function listaDeProdutos() {
-  const conexao = await fetch("http://localhost:3000/produtos");
+  const conexao = await fetch(
+    "https://json-server-teste-eight.vercel.app/produtos"
+  );
   const conexaoConvertida = await conexao.json();
   return conexaoConvertida;
 }
 async function adicionarProdutos(nome, preco, imagem, id) {
-  const conexao = await fetch("http://localhost:3000/produtos", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({
-      nome: nome,
-      preco: preco,
-      imagem: imagem,
-      id: id,
-    }),
-  });
+  const conexao = await fetch(
+    "https://json-server-teste-eight.vercel.app/produtos",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        nome: nome,
+        preco: preco,
+        imagem: imagem,
+        id: id,
+      }),
+    }
+  );
   const conexaoConvertida = await conexao.json();
   return conexaoConvertida;
 }
 async function excluirProdutos(id) {
-  const conexao = await fetch(`http://localhost:3000/produtos/${id}`, {
-    method: "DELETE",
-  });
+  const conexao = await fetch(
+    `https://json-server-teste-eight.vercel.app/produtos/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
   return conexao.ok;
 }
 
